5996804eafc0e2f866bf10ea38c02fb74b7c9aa0
kevin lang <langkevina@gmail.com>
2019-06-11 11:15:56 +0000

    Automatically backed up by Learn

---
+++ lib/project.rb
@@ -0,0 +1,12 @@
+class Project
+  attr_accessor :backers
+  attr_reader :title
+  def initialize(title)
+    @title = title
+    @backers = []
+  end
+  def add_backer(backer)
+    backer.back_project(self)
+    @backers << backer
+  end
+end
